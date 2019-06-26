ced95b5d649d5a38462bb8a1484c64c77a724f7f
gonsva <gonsva@gmail.com>
2019-06-26 12:08:36 +0100

    Project Progress

---
+++ MySQL scripts.sql
@@ -0,0 +1,15 @@
+drop table stats;
+insert into fstats values(7, 7, '51%', '47%', 22, 1, 0);
+
+
+select * from fstats;
+create table fstats(SID int(2), Fid int(2) references fighters, Striking_Accuracy char(3), Grappling_Accuracy char(3),  Wins int(2), 
+Losses int(2), Draws int(2));
+
+
+insert into fighters values(7, 'Daniel Costatsrmier');
+select * from fighters;
+create table fighters(fid int(2) primary key, name char(20));
+
+
+
